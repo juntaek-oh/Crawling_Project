72a1f93663b359cb7ec5700ac87d25da446c91f5
LRkms <some0506@naver.com>
2025-04-18 18:58:19 +0900

    Model_learning

--- model_learning.py
+++ model_learning.py
@@ -13,22 +13,21 @@ print(x_train.shape, y_train.shape)
 print(x_test.shape, y_test.shape)
 
 model = Sequential()
-model.add(Embedding(14974, 250)) # 형태소 15396 의미공간상 차원의 벡터화, 15396 형태소에 대해 좌표값이 생성
-#300 = 300차원으로 축소 이유는 차원의 저주 = 희소해진다 = 빈 공강이 생김 즉 학습이 안됨, 사진, 큐브, 데이터 손실을 최소화시키며 300차원으로 줄인다
+model.add(Embedding(14974, 250))
 model.build(input_shape=(None, 25))
-model.add(Conv1D(32, kernel_size=5, padding='same', activation='relu')) # 이미지는 2D, 문장은 1D(단어니까)
-model.add(MaxPool1D(pool_size=1)) #의미는 없지만 컨브레이어 뒤에는 항상 붙음
-model.add(LSTM(128, activation='tanh', return_sequences=True)) # RNN은 긴 문장에는 학습이 안됨
-model.add(Dropout(0,3))                # return_sequences=True 반복되는 값을 저장 뒤에 LSTM이 있으면 있어야함
+model.add(Conv1D(32, kernel_size=5, padding='same', activation='relu'))
+model.add(MaxPool1D(pool_size=1))
+model.add(LSTM(128, activation='tanh', return_sequences=True))
+model.add(Dropout(0,3))
 model.add(LSTM(128, activation='tanh', return_sequences=True))
 model.add(Dropout(0,3))
 model.add(LSTM(64, activation='tanh', return_sequences=True))
 model.add(Dropout(0,3))
 model.add(LSTM(64, activation='tanh'))
 model.add(Dropout(0,3))
-model.add(Flatten()) # 쭉 늘여놓고
+model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-model.add(Dense(9, activation='softmax')) #마지막 Dense 6개 카테고리가 6개이니
+model.add(Dense(9, activation='softmax'))
 model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adam',
